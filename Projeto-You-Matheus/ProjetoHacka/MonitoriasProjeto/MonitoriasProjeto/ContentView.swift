dda80352d4296443f4bbd68754a9279dabc080dd
matheushcp7 <129131653+matheushcp7@users.noreply.github.com>
2025-02-28 17:02:17 -0300

    Add files via upload

---
+++ Projeto-You-Matheus/ProjetoHacka/MonitoriasProjeto/MonitoriasProjeto/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  MonitoriasProjeto
+//
+//  Created by Turma02-22 on 26/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        TabView{
+            BotaoDeAguaView()
+                           .tabItem {
+                               Label("Água", systemImage: "drop.fill") 
+                           }
+            Home()
+                .tabItem {
+                    Label("", systemImage: "house.fill")
+                }
+            Monitorias()
+                .tabItem { 
+                    Label("", systemImage: "doc.richtext.fill.he")
+                }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Projeto-You-Matheus/ProjetoHacka/MonitoriasProjeto/Variaveis.swift
@@ -0,0 +1,12 @@
+//
+//  Variaveis.swift
+//  MonitoriasProjeto
+//
+//  Created by Turma02-17 on 28/02/25.
+//
+
+import Foundation
+import SwiftUI
+
+var minhaCor : Color = Color.pretoCinza
+var azul : Color = Color.azulBonito
