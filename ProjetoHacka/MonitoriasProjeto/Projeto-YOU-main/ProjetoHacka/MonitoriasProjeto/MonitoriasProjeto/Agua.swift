034fc09908aa0ce52e3b0bcc961ce3e06240684e
matheushcp7 <129131653+matheushcp7@users.noreply.github.com>
2025-02-28 14:39:27 -0300

    Add files via upload

---
+++ ProjetoHacka/MonitoriasProjeto/Projeto-YOU-main/ProjetoHacka/MonitoriasProjeto/MonitoriasProjeto/Agua.swift
@@ -0,0 +1,18 @@
+//
+//  Agua.swift
+//  MonitoriasProjeto
+//
+//  Created by Turma02-22 on 26/02/25.
+//
+
+import SwiftUI
+
+struct Agua: View {
+    var body: some View {
+        Text("Agua")
+    }
+}
+
+#Preview {
+    Agua()
+}
