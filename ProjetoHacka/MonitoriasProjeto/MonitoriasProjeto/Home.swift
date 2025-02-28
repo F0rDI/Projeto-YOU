6313ff37d015f7e33ce53b153e53858926f75ff1
Pedro Henrique Vilela <106275050+F0rDI@users.noreply.github.com>
2025-02-28 13:48:12 -0300

    Add files via upload

---
+++ ProjetoHacka/MonitoriasProjeto/MonitoriasProjeto/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  MonitoriasProjeto
+//
+//  Created by Turma02-22 on 26/02/25.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text("Tela Principal")
+    }
+}
+
+#Preview {
+    Home()
+}
