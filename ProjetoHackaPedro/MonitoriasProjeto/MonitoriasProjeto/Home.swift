2bc632daca264f5a57483f05c749daed3e75f1e5
Pedro Henrique Vilela <106275050+F0rDI@users.noreply.github.com>
2025-03-10 15:38:07 -0300

    Update Home.swift

--- ProjetoHackaPedro/MonitoriasProjeto/MonitoriasProjeto/Home.swift
+++ ProjetoHackaPedro/MonitoriasProjeto/MonitoriasProjeto/Home.swift
@@ -9,63 +9,100 @@ import SwiftUI
 
 struct Home: View {
     @State var sheet = false
+    @State var scrollindex: Int = 0
+    
     var body: some View {
         VStack{
             HStack{
                 Text("MEU DIA")
                     .foregroundColor(.white)
                     .frame(width: 130)
-                    .font(Font.custom("arial", size: 20))
+                    .font(Font.custom("Title", size: 20).bold())
                     .padding()
-                    .background(minhaCor)
+                    .background(azul)
                     .padding(.top, 1)
                     .shadow(color: minhaCor, radius: 5)
                 Spacer()
             }
+            
             Button(action: {}){
                 Text("INICIAR O CARDIO")
                     .padding()
+                    .font(Font.custom("Poppins-regular", size: 20).bold())
                     .foregroundColor(.white)
                     .frame(width: 250)
-                    .background(azul)
+                    .background(minhaCor)
                     .cornerRadius(10)
                     .padding(.top, 70)
                     .shadow(color: minhaCor, radius: 10)
                 
             }
+            
             HStack{
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack{
-                        ForEach(arrayData) { dat in
-                            VStack{
-                                VStack{
-                                    Text(dat.dat)
-                                        .padding(.top, 50)
-                                        .font(Font.custom("arial", size: 40))
-                                        .foregroundColor(.azulBonito)
-                                    Spacer()
-                                }
-                                HStack{
-                                    Spacer()
-                                    Text(dat.dia)
-                                        .foregroundStyle(Color.white)
-                                        .font(Font.custom("arial", size: 30))
-                                        .padding()
-                                }
-                            }
-                            .frame(width: 250, height: 150)
-                            .background(minhaCor)
-                            .cornerRadius(10)
-                            .shadow(color: minhaCor, radius: 3)
-                        }
-                        
-                        .padding(.top, 20)
-                        
+                Button (action: {
+                    if scrollindex > 0 {
+                        scrollindex -= 1
                     }
-                    .padding(.leading, 10)
-                    
+                }) {
+                    Image(systemName: "chevron.left")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding(8)
+                        .background(minhaCor)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                        .padding(.leading, 5)
+                        .shadow(color: minhaCor ,radius: 5)
+                }
+                
+                ScrollViewReader { proxy in
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack{
+                            ForEach(Array(arrayData.enumerated()), id: \.offset) {index, dat in
+                                VStack{
+                                        Text(dat.dat)
+                                            .padding(.top, 50)
+                                            .font(Font.custom("arial", size: 40).bold())
+                                            .foregroundColor(.white)
+                                    HStack{
+                                        Spacer()
+                                        Text(dat.dia)
+                                            .padding()
+                                            .foregroundStyle(azul)
+                                            .font(Font.custom("Times", size: 30))
+                                            .padding(.bottom, 15)
+                                    }
+                                }
+                                .frame(width: 250, height: 150)
+                                .background(minhaCor)
+                                .cornerRadius(10)
+                                .shadow(color: minhaCor, radius: 3)
+                                .id(index)
+                            }
+                        }
+                        .padding(.top, 20)
+                        .padding(.leading, 10)
+                    }
+                    .onChange(of: scrollindex) { novoNumero in
+                        withAnimation{
+                            proxy.scrollTo(novoNumero, anchor: .center)
+                        }
+                    }
                     
                 }
+                Button (action: {
+                    if scrollindex < arrayData.count - 1 {
+                        scrollindex += 1
+                    }
+                }) {
+                    Image(systemName: "chevron.right")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding(8)
+                        .background(minhaCor)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                        .padding(5)
+                        .shadow(color: minhaCor ,radius: 5)
+                }  
             }
             .padding(.top, 30)
             Spacer()
