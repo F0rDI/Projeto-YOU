6a465ef3140ce6c5a38d4209f0f99372a9fc3f3e
Pedro Henrique Vilela <106275050+F0rDI@users.noreply.github.com>
2025-03-11 16:42:40 -0300

    Update Atributos.swift

--- ProjetoHackaPedro/MonitoriasProjeto/MonitoriasProjeto/Atributos.swift
+++ ProjetoHackaPedro/MonitoriasProjeto/MonitoriasProjeto/Atributos.swift
@@ -19,36 +19,36 @@ struct Data : Identifiable{
 
 var arrayData = [Data(id: 1, dat: "01/02", dia: "Segunda"), Data(id: 2, dat: "02/02", dia: "Terca"), Data(id: 3, dat: "03/02", dia: "Quarta"), Data(id: 4, dat: "04/02", dia: "Quinta"), Data(id: 5, dat: "05/02", dia: "Sexta"), Data(id: 6, dat: "06/02", dia: "Sabado"), Data(id: 7, dat: "07/02", dia: "Domingo")]
 
-struct dadosDormir {
+struct dadosDormir: Decodable {
     var horasAcordou: Int?
     var horasDeitou: Int?
 }
 
-struct refeicao {
+struct refeicao: Decodable  {
     var horario: Int?
     var ingredientes: String?
     var preparo: String?
 }
 
-struct cronograma {
+struct cronograma: Decodable  {
     var horario: Int?
     var descricao: String?
 }
 
-struct tumtum {
+struct tumtum: Decodable {
     var horarioInicio: Int?
     var horarioFim: Int?
     var valor: Int?
 }
 
-struct planejamento {
+struct planejamento: Decodable {
     var cafeManha: refeicao?
     var almoco: refeicao?
     var jantar: refeicao?
     var treino: cronograma?
 }
 
-struct dados {
+struct dados: Decodable {
     var dia: Int?
     var agua: Bool?
     var sono: dadosDormir?
@@ -57,8 +57,9 @@ struct dados {
     var metasDiarias: planejamento?
 }
 
-struct pessoa: Decodable, Identifiable {
-    var _id: String?
+struct pessoa: Identifiable, Decodable {
+//    var id: ObjectIdentifier
+    var id: String?
     var _rev: String?
     var pesoMes: Double?
     var aguaTotal: Int?
