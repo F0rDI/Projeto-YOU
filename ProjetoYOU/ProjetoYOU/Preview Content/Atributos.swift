1ce071177bfb56e9e683e9688a85187a5ece970a
matheushcp7 <129131653+matheushcp7@users.noreply.github.com>
2025-03-12 15:46:30 -0300

    Update Atributos.swift

--- ProjetoYOU/ProjetoYOU/Preview Content/Atributos.swift
+++ ProjetoYOU/ProjetoYOU/Preview Content/Atributos.swift
@@ -19,36 +19,36 @@ struct Data : Identifiable{
 
 var arrayData = [Data(id: 1, dat: "01/02", dia: "Segunda"), Data(id: 2, dat: "02/02", dia: "Terca"), Data(id: 3, dat: "03/02", dia: "Quarta"), Data(id: 4, dat: "04/02", dia: "Quinta"), Data(id: 5, dat: "05/02", dia: "Sexta"), Data(id: 6, dat: "06/02", dia: "Sabado"), Data(id: 7, dat: "07/02", dia: "Domingo")]
 
-struct dadosDormir {
+struct dadosDormir: Codable {
     var horasAcordou: Date?
     var horasDeitou: Date?
 }
 
-struct refeicao {
+struct refeicao: Codable {
     var horario: Date?
     var ingredientes: String?
     var preparo: String?
 }
 
-struct cronograma {
+struct cronograma: Codable {
     var horario: Date?
     var descricao: String?
 }
 
-struct tumtum {
+struct tumtum: Codable {
     var horarioInicio: Date?
     var horarioFim: Date?
     var valor: Int?
 }
 
-struct planejamento {
+struct planejamento: Codable {
     var cafeManha: refeicao?
     var almoco: refeicao?
     var jantar: refeicao?
     var treino: cronograma?
 }
 
-struct dados {
+struct dados: Codable {
     var dia: Date?
     var agua: Bool?
     var sono: dadosDormir?
@@ -57,8 +57,9 @@ struct dados {
     var metasDiarias: planejamento?
 }
 
-//struct pessoa: Decodable {
- //   var pesoMes: Double?
-  //  var aguaTotal: Int?
- //   var dadosTotais: [dados]?
-//}
+struct pessoa: Codable {
+    var _id: String?         // Agora _id tem tipo definido
+    var pesoMes: Double?
+    var aguaTotal: Int?
+    var dadosTotais: [dados]?
+}
