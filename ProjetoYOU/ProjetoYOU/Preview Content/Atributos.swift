ec0e6b1285d7f8aa5c07c608bd8564f096284986
Luiz-Pena <lh1316245@gmail.com>
2025-03-12 16:19:06 -0300

    Update Atributos.swift

--- ProjetoYOU/ProjetoYOU/Preview Content/Atributos.swift
+++ ProjetoYOU/ProjetoYOU/Preview Content/Atributos.swift
@@ -19,46 +19,36 @@ struct Data : Identifiable{
 
 var arrayData = [Data(id: 1, dat: "01/02", dia: "Segunda"), Data(id: 2, dat: "02/02", dia: "Terca"), Data(id: 3, dat: "03/02", dia: "Quarta"), Data(id: 4, dat: "04/02", dia: "Quinta"), Data(id: 5, dat: "05/02", dia: "Sexta"), Data(id: 6, dat: "06/02", dia: "Sabado"), Data(id: 7, dat: "07/02", dia: "Domingo")]
 
-struct dadosDormir: Codable {
-    var horasAcordou: Date?
-    var horasDeitou: Date?
-}
 
-struct refeicao: Codable {
-    var horario: Date?
-    var ingredientes: String?
-    var preparo: String?
-}
-
-struct cronograma: Codable {
-    var horario: Date?
-    var descricao: String?
-}
-
-struct tumtum: Codable {
-    var horarioInicio: Date?
-    var horarioFim: Date?
-    var valor: Int?
-}
-
-struct planejamento: Codable {
-    var cafeManha: refeicao?
-    var almoco: refeicao?
-    var jantar: refeicao?
-    var treino: cronograma?
-}
-
-struct dados: Codable {
-    var dia: Date?
+struct dados {
+    var dia: Int?
     var agua: Bool?
-    var sono: dadosDormir?
-    var batimentos: tumtum?
+    
+    var horasAcordou: Int?
+    var horasDeitou: Int
+    
+    var InicioCardio: Int?
+    var FimCardio: Int?
+    var valor: Int?
+    
     var peso: Double?
-    var metasDiarias: planejamento?
+    
+    var horarioCafe: Int?
+    var ingredientesCafe: String?
+    
+    var horarioAlmo: Int?
+    var ingredientesAlmo: String?
+    
+    var horarioJantar: Int?
+    var ingredientesJantar: String?
+    
+    var horarioTreino: Int?
+    var descricaoTreino: String?
 }
 
-struct pessoa: Codable {
-    var _id: String?         // Agora _id tem tipo definido
+struct pessoa: Decodable, Hashable {
+    var _id: String? // Agora _id tem tipo definido
+    var _rev: String?
     var pesoMes: Double?
     var aguaTotal: Int?
     var dadosTotais: [dados]?
