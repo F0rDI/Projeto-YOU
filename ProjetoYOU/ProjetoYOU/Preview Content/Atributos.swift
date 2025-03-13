4d3b5bf8643f52ab7d881b6e26783a1cc099ee67
Luiz-Pena <lh1316245@gmail.com>
2025-03-13 13:54:26 -0300

    Update Atributos.swift

--- ProjetoYOU/ProjetoYOU/Preview Content/Atributos.swift
+++ ProjetoYOU/ProjetoYOU/Preview Content/Atributos.swift
@@ -20,7 +20,7 @@ struct Data : Identifiable{
 var arrayData = [Data(id: 1, dat: "01/02", dia: "Segunda"), Data(id: 2, dat: "02/02", dia: "Terca"), Data(id: 3, dat: "03/02", dia: "Quarta"), Data(id: 4, dat: "04/02", dia: "Quinta"), Data(id: 5, dat: "05/02", dia: "Sexta"), Data(id: 6, dat: "06/02", dia: "Sabado"), Data(id: 7, dat: "07/02", dia: "Domingo")]
 
 
-struct dados: Decodable, Hashable {
+struct dados: Codable, Hashable {
     var dia: Int?
     var agua: Bool?
     
@@ -46,7 +46,7 @@ struct dados: Decodable, Hashable {
     var descricaoTreino: String?
 }
 
-struct pessoa: Decodable, Hashable {
+struct pessoa: Codable, Hashable {
     var _id: String? // Agora _id tem tipo definido
     var _rev: String?
     var pesoMes: Double?
